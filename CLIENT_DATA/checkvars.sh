62c075764d007297c9a71fff39f114347586f3a1
Jens Boettge <boettge@mpi-halle.mpg.de>
2017-01-03 17:23:15 +0100

    first testing package Vivaldi 1.6.689.40-1

---
+++ CLIENT_DATA/checkvars.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#===========================================================
+# Check usage von variables in OPSI scripts (*.ins, *.inc)
+# Detect:
+#		repeatedly defined variables
+#		used but undefined variables
+#		unused but defined variables
+#
+# Version: 1.0
+# Jens Boettge <boettge@mpi-halle.mpg.de>  	2016-10-24 11:06:30 +0200
+#===========================================================
+
+VARS_DEFINED=$(grep -i -P -e 'def(Var|StringList)' variables.ins  | sed -e 's/^\s+//' -e 's/\s+/ /g' | awk -e '{print $2}' | sort )
+VARS_USED=$(cat *.ins *.inc 2>/dev/null| grep -v '^\s*;' | grep -i 'set\s*\$' | sed -e 's/^\s*//' | sed -e 's/\t/ /' | cut -f 2 -d " " | sort | uniq)
+
+echo "==============================="
+echo "repeatedly defined variables:"
+echo "==============================="
+echo "$VARS_DEFINED"  |uniq -c | awk -e '{print $1,$2}'| grep -v '^1 '
+echo -e ""
+VARS_DEFINED=$(echo "$VARS_DEFINED" | uniq)
+echo "==============================="
+echo "used but undefined variables"
+echo "==============================="
+for V in $VARS_USED; do [[ " ${VARS_DEFINED} " =~ "$V" ]] || echo "[$V]"; done
+echo -e ""
+echo "==============================="
+echo "unused but defined variables"
+echo "==============================="
+for V in $VARS_DEFINED; do [[ " ${VARS_USED} " =~ "$V" ]] || echo "[$V]"; done
+echo -e ""
+#echo "------------------------------------"
+#echo "$VARS_DEFINED"
+#echo "------------------------------------"
+#echo "$VARS_USED"
+#echo "------------------------------------"
