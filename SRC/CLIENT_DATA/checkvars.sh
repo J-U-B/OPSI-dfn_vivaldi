20751b3ab783761a2e884263c933e90ab57be2b3
Jens Boettge <jens.boettge@mpi-halle.mpg.de>
2022-10-10 15:47:44 +0200

    vivaldi 5.5.2805.35-37
    * product update ==> 5.5.2805.35
    
    vivaldi 5.5.2805.32-37
    * product update ==> 5.5.2805.32
    * slightly improved checkvars.sh

--- SRC/CLIENT_DATA/checkvars.sh
+++ SRC/CLIENT_DATA/checkvars.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
-#===========================================================
-# Check usage von variables in OPSI scripts 
+#=======================================================================
+# Check usage von variables in OPSI scripts
 # (*.ins, *.inc, *opsiscript, *.opsiinc)
 # Detect:
 #		repeatedly defined variables
 #		used but undefined variables
 #		unused but defined variables
 #
-# Version: 1.0.2
-# Jens Boettge <boettge@mpi-halle.mpg.de>  	2018-09-18 08:53:49 +0200
-#===========================================================
+# Version: 1.1.0
+# Jens Boettge <boettge@mpi-halle.mpg.de>  	2022-09-22 13:51:27 +0200
+#=======================================================================
 
-VARS_DEFINED=$(grep -i -P -e '^\s*def(Var|StringList)' variables.opsiinc checkinstance.opsiinc | sed -e 's/^\s+//' -e 's/\s+/ /g' | awk -e '{print $2}' | sort )
+VARS_VARINC=$(grep -i -P -e '^\s*def(Var|StringList)' variables.opsiinc  | sed -e 's/^\s+//' -e 's/\s+/ /g' | awk -e '{print $2}' | sort )
+VARS_DEFINED=$(cat *.opsi{inc,script}{,.in} 2>/dev/null | grep -i -P -e '^\s*def(Var|StringList)' | sed -e 's/^\s+//' -e 's/\s+/ /g' | awk -e '{print $2}' | sort )
 VARS_USED=$(cat *.opsi{inc,script}{,.in} 2>/dev/null| grep -v '^\s*;' | grep -i 'set\s*\$' | sed -e 's/^\s*//' | sed -e 's/\t/ /' | cut -f 2 -d " " | sort | uniq)
 
-echo "==============================="
+echo "=============================================="
+echo "variable definitions outside variables.opiinc:"
+echo "=============================================="
+for V in $VARS_DEFINED; do [[ " ${VARS_VARINC} " =~ "$V" ]] || echo "[$V]";  done
+echo -e ""
+echo "=============================================="
 echo "repeatedly defined variables:"
-echo "==============================="
+echo "=============================================="
 echo "$VARS_DEFINED"  |uniq -c | awk -e '{print $1,$2}'| grep -v '^1 '
 echo -e ""
 VARS_DEFINED=$(echo "$VARS_DEFINED" | uniq)
-echo "==============================="
+echo "=============================================="
 echo "used but undefined variables"
-echo "==============================="
+echo "=============================================="
 for V in $VARS_USED; do [[ " ${VARS_DEFINED} " =~ "$V" ]] || echo "[$V]"; done
 echo -e ""
-echo "==============================="
+echo "=============================================="
 echo "unused but defined variables"
-echo "==============================="
+echo "=============================================="
 for V in $VARS_DEFINED; do [[ " ${VARS_USED} " =~ "$V" ]] || echo "[$V]"; done
 echo -e ""
 #echo "------------------------------------"
